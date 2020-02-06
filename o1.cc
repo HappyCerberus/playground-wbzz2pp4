fa0748956f310c98f70d247e9d2f84bbe0e50d5a
RNDr. Simon Toth <kontakt@simontoth.cz>
2020-02-06 13:52:20 +0100

    O(1) maximum

--- o1.cc
+++ o1.cc
@@ -1,7 +1,9 @@
 #include <unordered_map>
+#include <unordered_set>
 #include <iostream>
 using namespace std;
 
+/*
 class Constant {
 	unordered_map<string, unsigned> _values;
 public:
@@ -47,11 +49,62 @@ string Constant::getMinKey() const {
 
 	return key;
 }
+*/
 
-void do_and_print_inc(const string& key) {
+class Constant {
+	unordered_map<string, unsigned> _values;
+	unordered_map<unsigned, unordered_set<string>> _buckets;
+	unsigned max = 0;
+public:
+	void inc(const string& key);
+	void dec(const string& key);
+	string getMaxKey() const;
+	string getMinKey() const;
+};
+
+void Constant::inc(const string& key) {
+	if (max == _values[key]) {
+		max++;
+	}
+
+	_values[key]++;
+	if (_values[key] > 1) {
+		_buckets[_values[key]-1].erase(key);
+	}
+	_buckets[_values[key]].insert(key);
 }
 
-void do_and_print_dec(const string& key) {
+void Constant::dec(const string& key) {
+	if (max == _values[key] && _buckets[_values[key]].size() == 1) {
+		max--;
+	}
+
+	_values[key]--;
+	_buckets[_values[key]+1].erase(key);
+	if (_values[key] != 0) {
+		_buckets[_values[key]].insert(key);
+	} else {
+		_values.erase(key);
+	}
+}
+
+string Constant::getMaxKey() const {
+	auto it = _buckets.find(max);
+	if (it == _buckets.end()) return string("");
+	return *it->second.begin();
+}
+
+string Constant::getMinKey() const {
+	unsigned min = numeric_limits<unsigned>::max();
+	string key = "";
+	for (auto v : _values) {
+		if (v.second < min) {
+			key = v.first;
+			min = v.second;
+		}
+	}
+
+	return key;
 }
 
 int main() {
